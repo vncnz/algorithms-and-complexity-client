a53c61744cfebc67629b93d7a739ff0224ca2e68
Vincenzo Minolfi <vincenzo.minolfi@gmail.com>
2025-07-08 23:27:36 +0200

    A bit of refactoring

--- game-ui.py
+++ game-ui.py
@@ -1,7 +1,7 @@
 import sys
 import json
 import select
-from PyQt5.QtWidgets import QApplication, QGraphicsScene, QGraphicsView, QGraphicsPolygonItem
+from PyQt5.QtWidgets import QApplication, QGraphicsScene, QGraphicsView, QGraphicsPolygonItem, QPushButton, QVBoxLayout, QWidget
 from PyQt5.QtGui import QPolygonF, QBrush, QColor, QPainter
 from PyQt5.QtCore import QPointF, QTimer
 
@@ -9,30 +9,9 @@ from game_ui_common import print_now, ClickablePolygon, read_stdin_line
 
 polygons = []
 colors = [QColor("#ff9999"), QColor("#99ccff")] # , QColor("#99ff99"), QColor("#cccccc"), QColor("#ffcc77")]
+scene = None
 
-def process_input(line):
-    try:
-        msg = json.loads(line)
-        # print_now(f"Received: {msg}")
-        for idx,v in enumerate(msg):
-            polygons[idx].update_color(colors[v])
-        # Esempio: potresti modificare un poligono in base al messaggio
-    # except json.JSONDecodeError:
-    #     print_now("Invalid JSON")
-    except Exception as e:
-        print(e)
-
-def main(blocking=True):
-    app = QApplication(sys.argv)
-    scene = QGraphicsScene()
-
-    #polygons = [
-        #[(50, 50), (150, 60), (120, 150), (60, 140)],
-        #[(200, 80), (300, 100), (280, 180), (210, 160)],
-        #[(100, 200), (180, 220), (160, 300), (90, 280)],
-        #[(250, 250), (320, 270), (310, 340), (260, 320)]
-    #]
-
+def first_draw ():
     polygon_defs = []
     for idx in range(16):
         sz = 30
@@ -48,19 +27,66 @@ def main(blocking=True):
         scene.addItem(poly)
         polygons.append(poly)
 
-    view = QGraphicsView(scene)
-    # view.setRenderHint(view.renderHints() | view.renderHints().Antialiasing)
-    view.setRenderHint(QPainter.Antialiasing)
-    view.setWindowTitle("Polygons with Click Events")
-    view.resize(400, 400)
-    view.show()
+def update_draw (data):
+    for idx,v in enumerate(data):
+        polygons[idx].update_color(colors[v])
 
-    # Timer per controllo stdin periodico
+def process_server_message(line):
+    try:
+        msg = json.loads(line)
+        # print_now(f"Received: {msg}")
+        update_draw(msg)
+    # except json.JSONDecodeError:
+    #     print_now("Invalid JSON")
+    except Exception as e:
+        print(e)
+
+def main(blocking=True):
+    global scene
+    app = QApplication(sys.argv)
+    scene = QGraphicsScene()
+    view = QGraphicsView(scene)
+    view.setFixedSize(400, 400)
+
+    #back_coo = (-150, -150)
+    #back_points = [
+    #    (back_coo[0]-50, back_coo[1]-10),
+    #    (back_coo[0]+50, back_coo[1]-10),
+    #    (back_coo[0]+50, back_coo[1]+10),
+    #    (back_coo[0]-50, back_coo[1]+10)
+    #]
+    #back = ClickablePolygon(-1, back_points, QColor("#ff8855"), text="Exit")
+    #scene.addItem(back)
+
+    #polygons = [
+        #[(50, 50), (150, 60), (120, 150), (60, 140)],
+        #[(200, 80), (300, 100), (280, 180), (210, 160)],
+        #[(100, 200), (180, 220), (160, 300), (90, 280)],
+        #[(250, 250), (320, 270), (310, 340), (260, 320)]
+    #]
+
+    first_draw()
+
+    # UI buttons
+    button = QPushButton("Exit")
+    button.clicked.connect(lambda: print_now("exit"))
+
+    # Layout
+    layout = QVBoxLayout()
+    layout.addWidget(view)
+    layout.addWidget(button)
+
+    window = QWidget()
+    window.setLayout(layout)
+    window.setWindowTitle("Flip")
+    window.show()
+
+    # Timer for checking stdin
     timer = QTimer()
     timer.timeout.connect(lambda: (
-        (line := read_stdin_line(blocking)) and process_input(line)
+        (line := read_stdin_line(blocking)) and process_server_message(line)
     ))
-    timer.start(100)  # ogni 100ms
+    timer.start(100) # ms
 
     sys.exit(app.exec_())
 
